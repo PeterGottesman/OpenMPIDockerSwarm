9111297fc2e9901fcc0b507cd7ccae7c4c2e2717
PeterGottesman <mygames1030@gmail.com>
2015-07-29 04:18:19 -0700

    Removed super user reqwuirement and added cor binding

--- runscript/runmaster.sh
+++ runscript/runmaster.sh
@@ -36,42 +36,42 @@ if [ ${NUMBER_SLAVE} -lt 1 ]; then
 fi
 
 echo "Building image..."
-sudo docker build -t ompiswarm ..
+docker build -t ompiswarm ..
 echo "Done"
 
 NUMBER_SLAVE=`expr ${NUMBER_SLAVE} - 1`
 NUMBER_CORES=$(nproc)
 
 echo "Initializing Master container...."
-masterid=$(sudo docker run -d -it -P --privileged=true --cpuset-cpus=0 -v /data:/data ompiswarm)
-masterip=`sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${masterid}`
+masterid=$(docker run -d -it -P --privileged=true --cpuset-cpus=0 -v /data:/data ompiswarm)
+masterip=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${masterid}`
 echo "done."
 
 echo "Initializing slave containers...."
 for (( c=0; c<$NUMBER_SLAVE; c++ ))
 do
-    core=c%$NUMBER_CORES
-    slaveid[${c}]=$(sudo docker run -d -it -P --privileged=true --cpuset-cpus=$CORE -v /data:/data ompiswarm)
-    slaveip[${c}]=`sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${slaveid[${c}]}`
+    CORE=c%$NUMBER_CORES
+    slaveid[${c}]=$(docker run -d -it -P --privileged=true --cpuset-cpus=$CORE -v /data:/data ompiswarm)
+    slaveip[${c}]=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${slaveid[${c}]}`
 done
 SLAVE_NUM=${#slaveid[@]}
 echo "done. ${SLAVE_NUM} slaves initialed!"
 
 echo "Create host file..."
-sudo echo ${masterip} > /data/hostfile
+echo ${masterip} > /data/hostfile
 for (( c=0; c<$SLAVE_NUM; c++ ))
 do
-    sudo echo ${slaveip[${c}]} >> /data/hostfile
+    echo ${slaveip[${c}]} >> /data/hostfile
 done
 echo "Done."
 
-sudo docker exec -it ${masterid} /bin/bash
+docker exec -it ${masterid} /bin/bash
 
 if [ "${OPTION}" == "holdmaster" ]; then
     for (( c=0; c<$SLAVE_NUM; c++))
     do
-        sudo docker stop ${slaveid[${c}]}
-        sudo docker rm ${slaveid[${c}]}
+        docker stop ${slaveid[${c}]}
+        docker rm ${slaveid[${c}]}
     done
     echo "Clear done."
     echo ""
@@ -80,8 +80,8 @@ if [ "${OPTION}" == "holdmaster" ]; then
     #else if [  ]; then
 
 else
-    sudo docker stop $(sudo docker ps -a -q)
-    sudo docker rm $(sudo docker ps -a -q)
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
     echo "Done."
     echo ""
     echo "All clean!"
